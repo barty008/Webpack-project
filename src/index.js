0cc7e0df7e025b3943b00b869eb76805c4ed0fd6
James <jamesb88@hotmail.co.uk>
2023-05-24 20:00:48 +0100

    animation complete

--- src/index.js
+++ src/index.js
@@ -9,21 +9,30 @@ function initialize() {
 }
 initialize()
 
+const main = document.getElementById("main")
 function navigationEvents() {
-  const main = document.getElementById("main")
-
+  const content = document.getElementById("content")
   const homeBtn = document.getElementById("homebtn")
   const menuBtn = document.getElementById("menubtn")
   const contactBtn = document.getElementById("contactbtn")
+  const tabs = document.querySelectorAll(".tab")
 
   menuBtn.addEventListener("click", loadMenu)
   homeBtn.addEventListener("click", loadHome)
   contactBtn.addEventListener("click", loadContact)
 
-  const tabs = document.querySelectorAll(".tab")
   tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
       main.classList.add("active")
+      flipTheCard()
+
+      // active.classList.add("puff-in-center")
     })
   })
+}
+function flipTheCard() {
+  main.classList.add("scale-in-top")
+  setTimeout(() => {
+    main.classList.remove("scale-in-top")
+  }, 1500)
 }
